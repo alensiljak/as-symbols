95b310d3a4c274d85ef7b11b5b6ebb7fca8f3b41
Alen Šiljak <dev@alensiljak.eu.org>
2023-01-23 12:46:50 +0100

    adding a search by ledger symbol

--- src/extensions.rs
+++ src/extensions.rs
@@ -6,13 +6,24 @@ use crate::SymbolMetadata;
 
 trait MyVec {
     /// Searches the list by symbol ("BVME:DEM") and returns an option.
-    fn find_by_full_symbol(&self, full_symbol: &str) -> Option<&SymbolMetadata>;
+    fn find_by_full_symbol(&self, search_term: &str) -> Option<&SymbolMetadata>;
+    fn find_by_ledger_symbol(&self, search_term: &str) -> Option<&SymbolMetadata>;
 }
 
 impl MyVec for Vec<SymbolMetadata> {
-    fn find_by_full_symbol(&self, full_symbol: &str) -> Option<&SymbolMetadata> {
+    fn find_by_full_symbol(&self, search_term: &str) -> Option<&SymbolMetadata> {
         self.iter().find_map(|sm| {
-            if sm.symbol_w_namespace() == full_symbol {
+            if sm.symbol_w_namespace() == search_term {
+                Some(sm)
+            } else {
+                None
+            }
+        })
+    }
+
+    fn find_by_ledger_symbol(&self, search_term: &str) -> Option<&SymbolMetadata> {
+        self.iter().find_map(|sm| {
+            if sm.ledger_symbol.is_some() && sm.ledger_symbol.as_ref().unwrap() == search_term {
                 Some(sm)
             } else {
                 None
@@ -43,5 +54,19 @@ mod tests {
 
         assert_eq!("EL49", actual.symbol.as_str());
         assert!(false);
+    }
+
+    #[test_log::test]
+    fn test_search_by_ledger_symbol() {
+        let mut a = SymbolMetadata::new();
+        a.namespace = Some("AMS".to_string());
+        a.symbol = "VETY".to_string();
+        a.ledger_symbol = Some("VETY_AS".to_string());
+
+        let list = vec![a];
+
+        let actual = list.find_by_ledger_symbol("VETY_AS").unwrap();
+
+        assert_eq!("VETY", actual.symbol);
     }
 }
